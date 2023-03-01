867ec9ac7602b885f6409a71d16ddad818b9623c
chanleeip <chanleeip@gmail.com>
2023-03-01 21:31:31 +0530

    commit the deploy

---
+++ NLP_deply/app.py
@@ -0,0 +1,32 @@
+import pickle
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import numpy
+with open('vectorizer00.pkl', 'rb') as f:
+    vectorizer = pickle.load(f)
+# Open the pkl file for reading
+with open('restaurant_review_sentiment_model_o.pkl', 'rb') as f:
+    # Load the contents of the file into a variable
+    data = pickle.load(f)
+corpus = []
+ps = PorterStemmer()
+stop_words = stopwords.words('english')
+stop_words.remove('no')
+
+# Now you can use the data variable in your program
+sttring=input()
+sttring=re.sub('[^a-zA-Z]',' ',sttring)
+sttring=sttring.lower()
+sttring=sttring.split()
+sttring=[ps.stem(j) for j in sttring if not j in set(stop_words)]
+sttring=' '.join(sttring)
+corpus.append(sttring)
+
+# print(corpus)
+xoxo=vectorizer.transform(corpus)
+predict=data.predict(xoxo.toarray())
+if predict==1:
+    print("positive")
+else:
+    print("negative")
---
+++ NLP_deply/main.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import nltk
+import re
+data_sheet=pd.read_csv("Restaurant_Reviews.tsv",delimiter="\t")
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+corpus=[ ]
+ps=PorterStemmer()
+stop_words=stopwords.words('english')
+stop_words.remove('no')
+for i in range (0,1000,1):
+    sttring=re.sub('[^a-zA-Z]',' ',data_sheet['Review'][i])
+    sttring=sttring.lower()
+    sttring=sttring.split()
+    sttring=[ps.stem(j) for j in sttring if not j in set(stop_words)]
+    sttring=' '.join(sttring)
+    corpus.append(sttring)
+from sklearn.feature_extraction.text import TfidfVectorizer
+tf=TfidfVectorizer(max_features=2000)
+x=tf.fit_transform(corpus).toarray()
+y=data_sheet.iloc[:,-1].values
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(  x, y, test_size=0.1)
+from sklearn.svm import LinearSVC
+clf=LinearSVC()
+clf.fit(X_train, Y_train)
+accuracy = clf.score(X_test,Y_test)
+print("Accuracy: {:.2f}%".format(accuracy * 100))
+import pickle
+
+# Save the model to disk
+filename = 'restaurant_review_sentiment_model_o.pkl'
+with open(filename, 'wb') as file:
+    pickle.dump(clf, file)
+filename_1='vectorizer00.pkl'
+with open(filename_1,'wb') as file:
+    pickle.dump(tf,file)
+
